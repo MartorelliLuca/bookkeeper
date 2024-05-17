20797855b0a2bd9997e6565245e4c155e9f985e1
Luca Martorelli <lucam0109@gmail.com>
2024-05-17 16:38:58 +0200

    Overall recap:
    - Added other dummy tests in different moduls.
    - Sort of started adding BA-DUA.
    - Chose the first Class for bookkeeper tests: WriteCacheTest.

---
+++ bookkeeper-server/src/test/java/WriteCacheTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.assertTrue;
+
+import org.apache.bookkeeper.bookie.storage.ldb.WriteCache;
+import org.junit.Test;
+import io.netty.buffer.ByteBufAllocator;
+
+public class WriteCacheTest {
+
+    @Test
+    public void dummyTest() {
+        // Create a WriteCache instance with some arbitrary parameters
+        WriteCache writeCache = new WriteCache(ByteBufAllocator.DEFAULT, 1024);
+
+        // Perform some dummy operations or assertions
+        assertTrue(writeCache.isEmpty());
+        writeCache.clear();
+        assertTrue(writeCache.isEmpty());
+
+        // Since this is just a dummy test, it always passes
+    }
+}
