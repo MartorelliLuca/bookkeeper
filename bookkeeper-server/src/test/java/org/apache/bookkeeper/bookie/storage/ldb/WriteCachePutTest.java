fee8a298770e9caa50b97acc1e6a63b9c875e18a
Luca Martorelli <lucam0109@gmail.com>
2024-05-28 15:20:13 +0200

    Added InvalidByteBuf
    Added the case where ByteBuf may be invalid.

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/WriteCachePutTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/WriteCachePutTest.java
@@ -6,6 +6,7 @@ import io.netty.buffer.UnpooledByteBufAllocator;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.apache.bookkeeper.bookie.storage.ldb.util.InvalidByteBuf;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -14,46 +15,50 @@ import java.util.Collection;
 public class WriteCachePutTest {
     private WriteCache writeCache;
     private final ByteBufAllocator byteBufAllocator;
-
+    private final InvalidByteBuf invalidByteBuf;
     private final long ledgerId;
     private final long entryId;
-    private final EntryType entryType;
+    private final ByteBufType entryType;
     private final boolean isExpectedException;
     private ByteBuf entry;
 
-    private enum EntryType {
+    private enum ByteBufType {
         NULL,
         VALID,
-        EMPTY
+        EMPTY,
+        INVALID
     }
-
-    //todo understand how to create an invalid ByteBuf
 
     @Parameterized.Parameters
     public static Collection<?> getParameters() {
         return Arrays.asList(new Object[][] {
        // ledgerID, entryID, entryType, exception
-                {-1, -1, EntryType.VALID, true},
-                {-1,  0, EntryType.VALID, true},
-                {0,   0, EntryType.VALID, false},
-                {0,  -1, EntryType.VALID, true},
-                {-1, -1, EntryType.NULL,  true},
-                {-1,  0, EntryType.NULL,  true},
-                {0,   0, EntryType.NULL,  true},
-                {0,  -1, EntryType.NULL,  true},
-                {-1, -1, EntryType.EMPTY, true},
-                {-1,  0, EntryType.EMPTY, true},
-                {0,   0, EntryType.EMPTY, false},
-                {0,  -1, EntryType.EMPTY, true}
+                {-1, -1, ByteBufType.INVALID, true},
+                {-1,  0, ByteBufType.INVALID, true},
+                {0,   0, ByteBufType.INVALID, true},
+                {0,  -1, ByteBufType.INVALID, true},
+                {-1, -1, ByteBufType.VALID,   true},
+                {-1,  0, ByteBufType.VALID,   true},
+                {0,   0, ByteBufType.VALID,   false},
+                {0,  -1, ByteBufType.VALID,   true},
+                {-1, -1, ByteBufType.NULL,    true},
+                {-1,  0, ByteBufType.NULL,    true},
+                {0,   0, ByteBufType.NULL,    true},
+                {0,  -1, ByteBufType.NULL,    true},
+                {-1, -1, ByteBufType.EMPTY,   true},
+                {-1,  0, ByteBufType.EMPTY,   true},
+                {0,   0, ByteBufType.EMPTY,   false},
+                {0,  -1, ByteBufType.EMPTY,   true}
         });
     }
 
-    public WriteCachePutTest(long ledgerId, long entryId, EntryType entryType, boolean isExpectedException) {
+    public WriteCachePutTest(long ledgerId, long entryId, ByteBufType entryType, boolean isExpectedException) {
         this.ledgerId = ledgerId;
         this.entryId = entryId;
         this.entryType = entryType;
         this.isExpectedException = isExpectedException;
         this.byteBufAllocator = UnpooledByteBufAllocator.DEFAULT;
+        this.invalidByteBuf = new InvalidByteBuf();
     }
 
     @Before
@@ -72,6 +77,9 @@ public class WriteCachePutTest {
                 break;
             case EMPTY:
                 this.entry = byteBufAllocator.buffer(entrySize);
+                break;
+            case INVALID:
+                this.entry= this.invalidByteBuf;
                 break;
         }
     }
