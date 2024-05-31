7c2923d1442a68110c4bfc00652da995220786b3
Luca Martorelli <lucam0109@gmail.com>
2024-05-31 17:27:18 +0200

    Added GetUnavailableEntriesTest, and already upgraded with Jacoco

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/util/GetUnavailableEntriesTest.java
@@ -0,0 +1,108 @@
+package org.apache.bookkeeper.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
+public class GetUnavailableEntriesTest {
+    private final AvailabilityOfEntriesOfLedger availabilityOfEntriesOfLedger;    // Object under test
+    private final long startEntry;    // ID of the entry from which to search
+    private final long lastEntry;     // ID ot the latest entry
+    private final BitSet bookieEntries;   // Entries in bookie
+    private final List<Long> expectedResult;  // Expected result
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        long lastBookieEntry = 9L;
+        List<Long> exception = Collections.singletonList(-1L);
+
+        BitSet validBitSet = new BitSet((int) lastBookieEntry);
+        long[] bookieEntries1 = {2L, 4L, 5L, 7L};
+
+        for (int i = 1; i < lastBookieEntry; i += 2) {
+            validBitSet.set(i);
+        }
+
+        List<Long> unavailableEntries1 = Arrays.asList(1L, 3L);
+        //BitSet con valori impostati oltre il range [startEntryId, lastEntryId]
+
+        BitSet validBitsetButExceeding = new BitSet((int)lastBookieEntry+10);   // sono presenti tutte le entry
+        validBitsetButExceeding.flip(0, (int)lastBookieEntry+11);
+
+        BitSet bitSetWithoutMatchingValues = new BitSet(17);
+        bitSetWithoutMatchingValues.flip(1, 2);
+
+        List<Long> unavailableEntries2 = new ArrayList<>();
+        for (long i = -1; i <= lastBookieEntry; i++) {
+            if (!Arrays.asList(2L, 4L, 5L, 7L).contains(i)) {
+                unavailableEntries2.add(i);
+            }
+        }
+
+        BitSet exceedingBitSet = new BitSet((int) lastBookieEntry + 10);
+        exceedingBitSet.flip(0, (int) lastBookieEntry + 11);
+
+
+        return Arrays.asList(new Object[][]{
+                {0, lastBookieEntry, validBitSet, bookieEntries1, unavailableEntries1},
+                {lastBookieEntry, lastBookieEntry, bitSetWithoutMatchingValues, bookieEntries1, new ArrayList<>()},
+                {lastBookieEntry, lastBookieEntry + 1, null, bookieEntries1, exception},
+                {-1, lastBookieEntry, validBitsetButExceeding, bookieEntries1, unavailableEntries2},
+                {lastBookieEntry, lastBookieEntry-1, validBitSet, bookieEntries1, new ArrayList<>()}
+        });
+    }
+
+
+    public GetUnavailableEntriesTest(long startEntryId, long lastEntryId, BitSet expectedEntries,
+                                     long[] bookieEntries, List<Long> expectedResult) {
+        this.startEntry = startEntryId;
+        this.lastEntry = lastEntryId;
+        this.expectedResult = new ArrayList<>(expectedResult);
+        this.bookieEntries = expectedEntries;
+
+        PrimitiveIterator.OfLong primitiveIterator = Arrays.stream(bookieEntries).iterator();
+        this.availabilityOfEntriesOfLedger = new AvailabilityOfEntriesOfLedger(primitiveIterator);
+    }
+
+    @Test
+    public void testAvailabilityOfEntries() {
+        List<Long> result;
+        try {
+            result = this.availabilityOfEntriesOfLedger.getUnavailableEntries(this.startEntry, this.lastEntry, this.bookieEntries);
+        } catch (NullPointerException e) {
+            result = new ArrayList<>();
+            result.add(-1L);
+        }
+
+        System.out.println(result);
+        assertEquals(this.expectedResult, result);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testGetUnavailableEntryBeforeClosing() throws Exception {
+
+        long[] content = {2L, 4L, 5L, 7L};
+        PrimitiveIterator.OfLong primitiveIterator = Arrays.stream(content).iterator();
+        AvailabilityOfEntriesOfLedger notClosedLedger = new AvailabilityOfEntriesOfLedger(primitiveIterator);
+
+        Field closedField = AvailabilityOfEntriesOfLedger.class.getDeclaredField("availabilityOfEntriesOfLedgerClosed");
+        closedField.setAccessible(true);
+        closedField.set(notClosedLedger, false);
+
+        notClosedLedger.getUnavailableEntries(startEntry, lastEntry, bookieEntries);
+    }
+
+    @Test
+    public void testEmptyBitSet() {
+        BitSet availabilityOfEntries = new BitSet();
+        List<Long> result = availabilityOfEntriesOfLedger.getUnavailableEntries(startEntry, lastEntry, availabilityOfEntries);
+        assertTrue(result.isEmpty());
+    }
+}
